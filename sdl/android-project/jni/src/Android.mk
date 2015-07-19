15bee75c7183d3af11cd16eee77bcb935450fb2f
Shin-ichi MORITA <shin1morita@gmail.com>
2015-07-20 08:45:24 +0900

    Added Android port.

---
+++ sdl/android-project/jni/src/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL2_PATH := ../SDL2
+SOURCE_PATH := ../../..
+
+LOCAL_CPPFLAGS := -std=c++1y -frtti -fexceptions -O3
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SOURCE_PATH)
+
+LOCAL_SRC_FILES := $(SDL2_PATH)/src/main/android/SDL_android_main.c \
+	$(SOURCE_PATH)/portable.cc \
+	$(SOURCE_PATH)/gl__image.cc \
+	$(SOURCE_PATH)/collada.cc \
+	$(SOURCE_PATH)/ball.cc \
+	$(SOURCE_PATH)/player.cc \
+	$(SOURCE_PATH)/stage.cc \
+	$(SOURCE_PATH)/computer.cc \
+	$(SOURCE_PATH)/main.cc
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf SDL2_mixer xml2
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ sdl/android-project/src/io/github/shin1m/Tennis.java
@@ -0,0 +1,5 @@
+package io.github.shin1m;
+
+import org.libsdl.app.SDLActivity;
+
+public class Tennis extends SDLActivity {}
