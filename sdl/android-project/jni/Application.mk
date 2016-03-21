79f8257283b414ce19175a25d2a59450fbfc29bb
Shin-ichi MORITA <shin1morita@gmail.com>
2016-03-22 07:26:13 +0900

    Upgraded NDK to the latest versions.

--- sdl/android-project/jni/Application.mk
+++ sdl/android-project/jni/Application.mk
@@ -1,4 +1,4 @@
-NDK_TOOLCHAIN_VERSION := 4.9
+NDK_TOOLCHAIN_VERSION := 5
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
